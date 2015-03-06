4b7c1d573206608d205aa8aabd7971878cdacafc
anil bhaila <anil.bhaila@tektak.com>
2015-03-06 18:51:45 +0545

    added second activity

--- app/src/main/java/com/anilbhaila/learn/app/Second.java
+++ app/src/main/java/com/anilbhaila/learn/app/Second.java
@@ -1,7 +1,10 @@
 package com.anilbhaila.learn.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by tektak on 3/6/15.
@@ -10,5 +13,15 @@ public class Second extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.second);
+
+        Button b = (Button) findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Second.this,MainActivity.class));
+            }
+        });
     }
 }
