d91887b7d8d5d9bc07ee74dfeb5b4653848dd2af
anil bhaila <anil.bhaila@tektak.com>
2015-03-06 18:37:52 +0545

    project created

---
+++ app/src/main/java/com/anilbhaila/learn/app/Second.java
@@ -0,0 +1,14 @@
+package com.anilbhaila.learn.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tektak on 3/6/15.
+ */
+public class Second extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
